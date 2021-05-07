4086f6c7d43b95ec0eb3fd1c168b42c740b71111
Daniel Flanagan <37907774+FlantasticDan@users.noreply.github.com>
2021-05-07 15:16:37 -0700

    Basketball Team Examples in Setup

--- flask/main.py
+++ flask/main.py
@@ -5,7 +5,7 @@ from images import Logos
 from manager import BasketballManager
 from bundle import bundle
 
-VERSION = 'v1.0.1 (0502021)'
+VERSION = 'v1.0.1 (05072021)'
 LOGOS = Logos()
 MANAGER = None # type: BasketballManager
 
